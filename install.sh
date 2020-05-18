0f52a25ad01ddf10a717289a57b119fe8c29b71d
BeckyArrowsmith <becky.arrowsmith@googlemail.com>
2020-05-18 14:29:03 +0100

    Update mac dotfiles

--- install.sh
+++ install.sh
@@ -17,8 +17,22 @@ brew bundle
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
+# Set zsh and oh-my-zsh preferences
+
+
 # Create a Code directory
 mkdir $HOME/Code
+
+# Install vscodium and iterm 2
+brew cask install vscodium
+brew cask install iterm2
+
+codesign --sign - --force --deep /Applications/VSCodium.app # Stops MacOS being daft about opening vscodium
+xattr -d com.apple.quarantine /Applications/VSCodium.app 
+
+# Install Slack and Signal
+brew cask install slack
+brew cask install signal
 
 # Set OS X preferences
 # We will run this last because this will reload the shell
